4efcdc38386e9b57c6167c1b0e874a7651873065
TranTam <tamvaa1306@gmail.com>
2022-10-02 16:22:47 +0700

    twosum

--- javascript/1-two-sum.js
+++ javascript/1-two-sum.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+// Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution,
+// and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+const findIndices = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        console.log([i, j]);
+        return [i, j];
+      }
+    }
+  }
+};
+
+findIndices([3, 7, 4, 2, 1, 6, 9], 9);
